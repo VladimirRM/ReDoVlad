0d542669e5eabe9efb376655996078fda9e855cd
Vladimir RM <shvdrummer@gmail.com>
2022-11-08 15:07:54 +0200

    inport usestate & crete const todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import React,{useState} from 'react';
 import './App.css';
+import ToDo from './ToDo'
+import ToDoForm from './ToDoForm'
 
 function App() {
+  const [todos, setTodos]= useState( )
   return (
     <div className="App">
-   
+    <ToDo/>
+    <ToDoForm/>
     </div>
   );
 }
