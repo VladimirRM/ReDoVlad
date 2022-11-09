fe7f5debb6119d7f3aa3dfafafa9459821dc181e
Vladimir RM <shvdrummer@gmail.com>
2022-11-09 15:31:40 +0200

    e.preventDefault() in handlesubmit

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 
 const ToDoForm = ({addTask}) => {
   const [userInput, setUserInput] = useState("");
-  const handleChange =()=>{
+  const handleChange =(e)=>{
     setUserInput(e.currentTarget.value)
   }
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  };
 const handleKeyPress =()=>{
 
   }
