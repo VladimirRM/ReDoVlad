16f9b6e113d6723110b7fdd1384d422d94c3f33e
Vladimir RM <shvdrummer@gmail.com>
2022-12-09 09:32:29 +0200

    add main import in App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import ToDo from "./ToDo";
+import ToDoForm from "./ToDoForm";
+import './index.css'
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  return <div className="App"></div>;
+};
+
+export default App;
