6aeb5167ef67a5f778be52b499f90ca23573ba25
Vladimir RM <shvdrummer@gmail.com>
2022-12-09 09:56:01 +0200

    create todo component

--- src/ToDo.js
+++ src/ToDo.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const ToDo = ({todo,removeTask,toggleTask}) => {
   return (
-    <div>
-      
+    <div className='item-todo' key={todo.id}>
+      <div className={todo.complete ?'item-text strike':'item-text'} onClick={()=>toggleTask(todo.id)}></div>
+      <div className="item-delete" onClick={()=>removeTask(todo.id)}>X</div>
     </div>
   )
 }
