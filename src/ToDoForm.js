1dd054730956838831b29bf3819f998ecd427e2d
Vladimir RM <shvdrummer@gmail.com>
2022-12-09 09:48:23 +0200

    create func handleSubmit

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 
 const ToDoForm = ({ addTask }) => {
 
-const handleChange =()=>{
-  
+const handleChange =(e)=>{
+    setUserInput(e.currentTarget.value)
 }
-const handleSubmit =()=>{
+const handleSubmit =(e)=>{
+  e.preventDefault()
+  addTask(userInput)
 
 }
 const handleKeyPress=()=>{
