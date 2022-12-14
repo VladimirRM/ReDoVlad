78f688265dc2c57c699a842ffc9dfa6bf0bd7e74
Vladimir RM <shvdrummer@gmail.com>
2022-12-14 15:10:04 +0200

    add header

--- src/App.js
+++ src/App.js
@@ -1,47 +1,16 @@
-import React, { useState } from "react";
-import ToDo from "./ToDo";
-import ToDoForm from "./ToDoForm";
-import "./index.css";
+import React from 'react'
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-
-  const addTask = (userInput) => {
-    if (userInput) {
-      const newItem = {
-        id: Math.random().toString(39).substring(2, 9),
-        task: userInput,
-        complete: false,
-      };
-      setTodos([...todos, newItem]);
-    }
-  };
-
-  const removeTask = (id) => {
-    setTodos([
-      ...todos.filter((todo)=>todo.id !==id)
-    ])
-  };
-
-  const handleToggle = () => {};
   return (
-    <div className="App">
+    <div>
       <header>
-        <h1>Todo List:{todos.length}</h1>
-      </header>
-      <ToDoForm addTask={addTask} />
-      {todos.map((todo) => {
-        return (
-          <ToDo
-            key={todo.id}
-            todo={todo}
-            removeTask={removeTask}
-            toggleTask={handleToggle}
-          />
-        );
-      })}
-    </div>
-  );
-};
+        <h1>
+          
+        </h1>
 
-export default App;
+      </header>
+    </div>
+  )
+}
+
+export default App
