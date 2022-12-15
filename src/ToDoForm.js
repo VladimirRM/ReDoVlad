adcbf45967b230b4459d99fccd68020e09a13574
Vladimir RM <shvdrummer@gmail.com>
2022-12-15 09:37:42 +0200

    add  const userInput

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const ToDoForm = ({addTask}) => {
+
+  const [userInput,setUserInput]= useState('')
   return (
     <form onSubmit={handleSubmit}>
+      <input type="text"
+      onChange={handleChange}
+
       
+      
+      
+      />
     </form>
   )
 }
