1b10df9c0c212fbc9edc740984d145f98f32d158
Vladimir RM <shvdrummer@gmail.com>
2022-12-15 09:33:17 +0200

    add main const in App

--- src/App.js
+++ src/App.js
@@ -6,16 +6,35 @@ import "./index.css";
 const App = () => {
   const [todos, setTodos] = useState("");
 
+  const addTask =()=>{
+    
+  }
+  const removeTask =()=>{
+
+  }
+  const handleToggle =()=>{
+
+  }
+
   return (
     <div>
       <header>
         <h1>Todo List:{todos.length}</h1>
       </header>
+
+      <ToDoForm addTask={addTask}/>
+      {todos.map((todo)=>
+      <ToDo
+      key={todo.id}
+      removeTask={removeTask}
+      toggleTask={handleToggle}
+      
+      
+      
+      
+      />
+      )}
     </div>
-     
-
-
-
   );
 };
 
