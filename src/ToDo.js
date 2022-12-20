f5be32945402771efb158a6e25aa01748b7cf7b1
Vladimir RM <shvdrummer@gmail.com>
2022-12-20 10:13:36 +0200

    add button delete in todo

---
+++ src/ToDo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ToDo = ({todo,removeTask,handleToggle}) => {
+  return (
+    <div key={todo.id}>
+      <div></div>
+      <div>Delete</div>
+      
+    </div>
+  )
+}
+
+export default ToDo
