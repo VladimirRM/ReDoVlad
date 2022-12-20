bd512961736f6e0e573daaa1c25a96100b71c3c8
Vladimir RM <shvdrummer@gmail.com>
2022-12-20 10:38:10 +0200

    cleane App code

--- src/App.js
+++ src/App.js
@@ -1,50 +1,11 @@
-import React, { useState } from "react";
-import ToDo from "./ToDo";
-import ToDoForm from "./ToDoForm";
-import "./index.css";
+import React, from 'react'
 
 const App = () => {
-  const [todos, setTodos] = useState("");
-
-  const addTask = (userInput) => {
-    if (userInput) {
-      const newTodo = {
-        id: Math.random().toString(36).substring(2, 9),
-        task: userInput,
-        complete: true,
-      };
-      setTodos([...todos, newTodo]);
-    }
-  };
-  const removeTask = (id) => {
-    setTodos([...todos.filter((todo) => (todo.id = !id))]);
-  };
-  const handleToggle = (id) => {
-    setTodos([
-      ...todos.map((todo) =>
-        todo.id ? { ...todo, complete: !todo.complete } : { ...todo }
-      ),
-    ]);
-  };
-
   return (
     <div>
-      <header>
-        <h1>Todo List:{todos.length}</h1>
-      </header>
-
-      <ToDoForm addTask={addTask} />
-      {todos.map((todo) => {
-        return (
-          <ToDo
-            key={todo.id}
-            removeTask={removeTask}
-            toggleTask={handleToggle}
-          />
-        );
-      })}
+      
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
