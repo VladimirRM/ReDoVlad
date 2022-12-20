c72df47589e7318a7a8338ba5648a5c3870d8419
Vladimir RM <shvdrummer@gmail.com>
2022-12-20 10:11:30 +0200

    create func handleKeyPress

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -1,18 +1,20 @@
 import React, { useState } from "react";
 
-const ToDoForm = ({ addTask }) => {
+const ToDoForm = ({ addTask, todo }) => {
   const [userInput, setUserInput] = useState("");
 
-
-  const handleChange =(e)=>{
-         setUserInput(e.currentTarget.value)
-  }
-  const handleSubmit =(e)=>{
-         addTask(userInput)
-  }
-  const handleKeyPress =()=>{
-
-  }
+  const handleChange = (e) => {
+    setUserInput(e.currentTarget.value);
+  };
+  const handleSubmit = (e) => {
+    addTask(userInput);
+    setUserInput("");
+  };
+  const handleKeyPress = (e) => {
+    if (e.key === " Enter") {
+      handleSubmit(e);
+    }
+  };
   return (
     <form onSubmit={handleSubmit}>
       <div key={todo.id}>
