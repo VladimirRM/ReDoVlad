457d219d3abb67a5d80293e61f918bda130ed485
Vladimir RM <shvdrummer@gmail.com>
2022-12-26 13:30:43 +0200

    add main const in todoform

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -3,6 +3,16 @@ import React, { useState } from "react";
 const ToDoForm = ({ addTask, todo }) => {
   const [userInput, setUserInput] = useState("");
 
+  const handleSubmit =()=>{
+    
+  }
+  const handleChange =()=>{
+
+  }
+  const handleKeyPress =()=>{
+
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <input
